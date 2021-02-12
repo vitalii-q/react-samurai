bb2e10a8df34c21fd6e1485cde0fb83e29165162
Vitaly <vitalicheg28@mail.ru>
2021-02-12 22:01:17 +0300

    02/12/2021

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom"; // пакет роутеров
 
 import './css/main-style.css';
 import './css/owl.carousel.css';
 import './css/style.css';
+import './css/style_custom.css';
 
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Content from "./components/Content";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Profile from "./components/Profile/Profile";
+import Friends from "./components/Friends/Friends";
+import Posts from "./components/Posts/Posts";
+import PostPage from "./components/Post/Post";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {updateNewPostText} from "./redux/state";
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="app-wrapper">
         <Header />
@@ -22,7 +29,21 @@ const App = () => {
                     </div>
 
                     <div className="col-md-9">
-                        <Content />
+                        {/*<Route path='/blog' component={PostPage} />*/}
+                        {/*<Posts />*/}
+
+                        <Route path='/profile' render={ () => <Profile
+                            state={props.state.profilePage}
+                            addPost={props.addPost}
+                            updateNewPostText={props.updateNewPostText}
+                        />
+                        } />
+                        <Route path='/friends' render={ () => <Friends state={props.state.friendsPage} />} />
+                        <Route path='/messages' render={ () => <Dialogs
+                            state={props.state.messagesPage}
+                            addMessage={props.addMessage}
+                            updateNewMessageText={props.updateNewMessageText}
+                        />} />
                     </div>
                 </div>
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import store from './redux/state';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={store.getState()}
+                     addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}
+                     addMessage={store.addMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)}
+                />
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+rerenderEntireTree(store._state);
+
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
